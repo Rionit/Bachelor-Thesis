4925ce9f9db074c5aed42cfb56d314f9161d26f3
Filip Doležal <fidokadan@gmail.com>
2025-03-26 12:03:58 +0100

    Robot can be placed on the ground

--- Assets/Scripts/RobotController.cs
+++ Assets/Scripts/RobotController.cs
@@ -6,13 +6,17 @@ using UnityEngine.AI;
 
 public class RobotController : MonoBehaviour
 {
+    public LayerMask layersToHit;
+    
     private NavMeshAgent agent;
     private Animator animator;
+    private Camera cam;
     
     private void Start()
     {
         animator = GetComponent<Animator>();
         agent = GetComponent<NavMeshAgent>();
+        cam = Camera.main;
         
         GoTo(Vector3.zero);
     }
@@ -26,6 +30,28 @@ public class RobotController : MonoBehaviour
                 // Destination reached
                 animator.SetTrigger("TrStop");
             }
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            RaycastPosition(Input.mousePosition);
+        }
+        else if (Input.touchCount > 0)
+        {
+            RaycastPosition(Input.GetTouch(0).position);
+        }
+    }
+
+    private void RaycastPosition(Vector3 screenPosition)
+    {
+        Ray ray = cam.ScreenPointToRay(screenPosition);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, layersToHit))
+        {
+            Debug.DrawRay(ray.origin, ray.direction * hit.distance, Color.red, 5);
+            Debug.Log(hit.collider.gameObject.name + " was hit!");
+            transform.position = hit.point;
         }
     }
 
